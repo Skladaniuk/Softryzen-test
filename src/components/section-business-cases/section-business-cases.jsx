2ea15466e468e8d8f0100053a14b65b8bcaadef7
Oleksii Skladaniuk <93865262+Skladaniuk@users.noreply.github.com>
2023-04-30 22:58:59 +0300

    added lightbox

--- src/components/section-business-cases/section-business-cases.jsx
+++ src/components/section-business-cases/section-business-cases.jsx
@@ -1,151 +1,95 @@
 import { img } from "./img";
 import scss from "./section-business-cases.module.scss";
-
-
+import React, { useState } from "react";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 export const SectionBusinessCases = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const images = [
+    {
+      srcSet: `${img.imgcases1webp} 1x, ${img.imgcases1webp2x} 2x ,${img.imgcases1jpg} 1x, ${img.imgcases1jpg2x} 2x`,
+      src: img.imgcases1webp,
+      thumbnailSrc: img.imgcases1jpg,
+      alt: "case1",
+    },
+    {
+      srcSet: `${img.imgcases2webp} 1x, ${img.imgcases2webp2x} 2x ,${img.imgcases2jpg} 1x, ${img.imgcases2jpg2x} 2x`,
+      src: img.imgcases2webp,
+      thumbnailSrc: img.imgcases2jpg,
+      alt: "case2",
+    },
+    {
+      srcSet: `${img.imgcases3webp} 1x, ${img.imgcases3webp2x} 2x ,${img.imgcases3jpg} 1x, ${img.imgcases3jpg2x} 2x`,
+      src: img.imgcases3webp,
+      thumbnailSrc: img.imgcases3jpg,
+      alt: "case3",
+    },
+    {
+      srcSet: `${img.imgcases4webp} 1x, ${img.imgcases4webp2x} 2x ,${img.imgcases4jpg} 1x, ${img.imgcases4jpg2x} 2x`,
+      src: img.imgcases4webp,
+      thumbnailSrc: img.imgcases4jpg,
+      alt: "case4",
+    },
+    {
+      srcSet: `${img.imgcases5webp} 1x, ${img.imgcases5webp2x} 2x ,${img.imgcases5jpg} 1x, ${img.imgcases5jpg2x} 2x`,
+      src: img.imgcases5webp,
+      thumbnailSrc: img.imgcases5jpg,
+      alt: "case5",
+    },
+    {
+      srcSet: `${img.imgcases6webp} 1x, ${img.imgcases6webp2x} 2x ,${img.imgcases6jpg} 1x, ${img.imgcases6jpg2x} 2x`,
+      src: img.imgcases2webp,
+      thumbnailSrc: img.imgcases6jpg,
+      alt: "case6",
+    },
+  ];
   return (
     <div className={scss.sectionCases}>
-      <div className={scss.casesWrapper}>
-        <p className={scss.title}>This what we do</p>
-        <h2>Business Cases</h2>
-        <p className={scss.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
-          sapiente!
-        </p>
-        <div>
-          <ul className={scss.imgWrapper}>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases1webp} 1x, ${img.imgcases1webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases1jpg} 1x, ${img.imgcases1jpg} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
-                <img
-                  className={scss.picture}
-                  src={img.imgcases1jpg}
-                  alt="case1"
-                  width="280px"
-                />
-              </picture>
-            </li>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases2webp} 1x, ${img.imgcases2webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases2jpg} 1x, ${img.imgcases2jpg2x} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
+      <p className={scss.title}>This what we do</p>
+      <h2>Business Cases</h2>
+      <p className={scss.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+        sapiente!
+      </p>
 
-                <img
-                  className={scss.picture}
-                  src={img.imgcases2jpg}
-                  alt="case2"
-                  width="280px"
-                />
-              </picture>
-            </li>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases3webp} 1x, ${img.imgcases3webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases3jpg} 1x, ${img.imgcases3jpg2x} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
-
-                <img
-                  className={scss.picture}
-                  src={img.imgcases3jpg}
-                  alt="case3"
-                  width="280px"
-                />
-              </picture>
-            </li>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases4webp} 1x, ${img.imgcases4webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases4jpg} 1x, ${img.imgcases4jpg2x} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
-
-                <img
-                  className={scss.picture}
-                  src={img.imgcases4jpg}
-                  alt="case4"
-                  width="280px"
-                />
-              </picture>
-            </li>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases5webp} 1x, ${img.imgcases5webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases5jpg} 1x, ${img.imgcases5jpg2x} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
-
-                <img
-                  className={scss.picture}
-                  src={img.imgcases5jpg}
-                  alt="case5"
-                  width="280px"
-                />
-              </picture>
-            </li>
-            <li className={scss.imgItem}>
-              <picture className={scss.picture}>
-                <source
-                  srcSet={`${img.imgcases6webp} 1x, ${img.imgcases6webp2x} 2x `}
-                  type="image/webp"
-                  width="280px"
-                />
-                <source
-                  srcSet={`${img.imgcases6jpg} 1x, ${img.imgcases6jpg2x} 2x `}
-                  type="image/jpg"
-                  width="280px"
-                />
-
-                <img
-                  className={scss.picture}
-                  src={img.imgcases6jpg}
-                  alt="case6"
-                  width="280px"
-                />
-              </picture>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <ul className={scss.imgWrapper}>
+        {images.map((image, index) => (
+          <li className={scss.imgItem} key={index}>
+            <img
+              className={scss.picture}
+              srcSet={image.srcSet}
+              width="280px"
+              src={image.thumbnailSrc}
+              alt={image.alt}
+              onClick={() => {
+                setCurrentImage(index);
+                setIsOpen(true);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[currentImage].src}
+          nextSrc={images[(currentImage + 1) % images.length].src}
+          prevSrc={
+            images[(currentImage + images.length - 1) % images.length].src
+          }
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setCurrentImage((currentImage + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setCurrentImage((currentImage + 1) % images.length)
+          }
+          imageTitle={images[currentImage].alt}
+        />
+      )}
     </div>
   );
 };
 
-
-// 
+//
